b96f7b530931f8841b841eef779c462bdcb93ef9
Artem <gg.ize.pizi@gmail.com>
2024-11-17 02:17:16 +0300

    17/11

---
+++ module_4_1.py
@@ -0,0 +1,21 @@
+import fake_math as m1
+import true_math as m2
+
+fake_divide = m1.divide
+true_divide = m2.divide
+
+result1 = fake_divide(69, 3)
+
+result2 = fake_divide(3, 0)
+
+result3 = true_divide(49, 7)
+
+result4 = true_divide(15, 0)
+
+print(result1)
+
+print(result2)
+
+print(result3)
+
+print(result4)
