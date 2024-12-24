a0029f52d90cc18ff0c05d654c84da7f2199f8a7
Artem <gg.ize.pizi@gmail.com>
2024-12-24 22:24:13 +0300

    24/12

--- module_7_4.py
+++ module_7_4.py
@@ -14,7 +14,7 @@ print("Итого сегодня в командах участников: %s и
 
 print("Команда Волшебники данных решила задач: {} !".format(score_2))
 
-print(" Волшебники данных решили задачи за {} с !".format(team1_time))
+print("Волшебники данных решили задачи за {} с. !".format(team1_time))
 print(f'Команды решили {score_1} и {score_2} задач.')
 
 if score_1 > score_2 or score_1 == score_2 and team1_time < team2_time:
@@ -24,5 +24,5 @@ elif score_1 < score_2 or score_1 == score_2 and team1_time > team2_time:
 else:
     challenge_result = 'Ничья!'
 
-print(f'Результат битвы: {challenge_result} !')
-print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу!.')
+print(f'Результат битвы: {challenge_result} ')
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {time_avg} секунды на задачу.')
