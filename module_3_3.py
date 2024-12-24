4ec405c9aec4efc19f280e18992f302a0008d48a
Artem <gg.ize.pizi@gmail.com>
2024-12-24 22:10:43 +0300

    19/12

--- module_3_3.py
+++ module_3_3.py
@@ -14,5 +14,6 @@ values_dict = {'a' : 1, 'b' : 'строка', 'c' : True}
 print_params(*values_list)
 print_params(**values_dict)
 
+
 values_list_2 = (54.32, 'Строка')
 print_params(*values_list_2, 42)
