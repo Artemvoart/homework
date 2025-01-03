1debe74852a6c205e9052a7e99edb352ff1c6c63
Artem <gg.ize.pizi@gmail.com>
2025-01-03 23:25:01 +0300

    02/01

--- module_5_3.py
+++ module_5_3.py
@@ -4,11 +4,9 @@ class House:
         self.number_of_floors = number_of_floors
 
     def go_to(self, new_floor):
-
         if not 1 <= new_floor <= self.number_of_floors:
             print("Такого этажа не существует")
             return
-
         for floor in range(1, new_floor + 1):
             print(floor)
 
@@ -18,7 +16,6 @@ class House:
     def __str__(self):
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
-
     def __eq__(self, other):
         if isinstance(other, House):
             return self.number_of_floors == other.number_of_floors
@@ -27,37 +24,42 @@ class House:
     def __lt__(self, other):
         if isinstance(other, House):
             return self.number_of_floors < other.number_of_floors
+        return False
 
     def __le__(self, other):
         if isinstance(other, House):
             return self.number_of_floors <= other.number_of_floors
+        return False
 
     def __gt__(self, other):
         if isinstance(other, House):
             return self.number_of_floors > other.number_of_floors
+        return False
 
     def __ge__(self, other):
         if isinstance(other, House):
             return self.number_of_floors >= other.number_of_floors
+        return False
 
     def __ne__(self, other):
         if isinstance(other, House):
             return self.number_of_floors != other.number_of_floors
+        return False
 
     def __add__(self, value):
-        self.number_of_floors += value
+        if isinstance(value, int):
+            self.number_of_floors += value
         return self
 
     def __radd__(self, other):
-        return self + other
-
+        return self.__add__(other)
 
     def __iadd__(self, other):
-        return self + other
+        return self.__add__(other)
+
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)
-
 print(h1)
 print(h2)
 print(h1 == h2) # __eq__
@@ -70,9 +72,9 @@ h2 = 10 + h2 # __radd__
 print(h2)
 print(h1 > h2) # __gt__
 print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print(h1 < h2) # lt__
+print(h1 <= h2) # __le
+print(h1 != h2) # ne
 
 
 
